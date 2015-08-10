98f41574219a4a1927d3674203a19e16615cce0a
bolo07 <bolo4321@gmail.com>
2015-08-10 23:50:44 +0200

    poniedzialek koniec

--- Multi/heurystyki.cs
+++ Multi/heurystyki.cs
@@ -64,14 +64,67 @@ namespace Multi
 
 
         ///////////////////////////////////////////KROK 2///////////////////////////////////////////////
-            
-            List<bool> vis = new List<bool>();	//wierzcholek odwiedzony							//obliczenie minimalnego drzewa spinajacego z grafu kpp
-            List<int> waga = new List<int>();  //delay polaczenia									//metryka jest delay
-            List<int> waga_k = new List<int>();//koszt 
+            //////////////////////obliczenie minimalnego drzewa spinajacego z grafu kpp 
+            //////////////////////metryka jest delay
+
+            bool[] vis = new bool [n] ;	     //wierzcholek odwiedzony							
+            double[] waga = new double[n];  //delay polaczenia									
+            double[] waga_k = new double[n];      //koszt 
+
+            Dictionary<siec, siec> kopiec = new Dictionary<siec,siec>(n);
+
+            for(int i=0; i<n; i++)
+                {
+                 waga_k[i]=0;
+                 waga[i]= 0;				//inicjowanie tablic
+                 vis[i]= false;
+                }
+
+            int v = 0, waga_mst = 0;
+
+            siec[] mst = new siec[n];
+
+            for (int i = 0; i < n; i++)
+                 mst[i] = null;
 
 
-            HashSet<siec> kopiec = new HashSet<siec>();
-          
+            vis[0] = true;				//wierzcholek startowy (nadawca)
+            waga[odbiorcy[0]] = 0;
+            waga_k[odbiorcy[0]] = 0;
+
+            kopiec.Clear();
+    
+            if(!vis[pomoc.to] )
+		        {	
+			if( waga[pomoc.to]+ pomoc.delay < delta  )
+			{
+				vis[pomoc.to]=true;				//dodaje nowa krawedz do drzewa
+				waga[pomoc.to]= pomoc.delay;
+				waga_k[pomoc.to]= waga_k[pomoc.to]+pomoc.cost;
+
+				//nowy2 = new wezel(pomoc->koszt, pomoc->delay, pomoc->pi, pomoc->v, pomoc->id, mst[pomoc->v] );
+				//mst[pomoc->v]=nowy2;
+				siec nowy2 = new siec(pomoc.cost, pomoc.delay, pomoc.to, pomoc.to, pomoc.id, mst[pomoc.from] );
+				mst[pomoc.from]=nowy2;
+				v=pomoc.to;
+			}//else 
+				//{
+				//kopiec.erase(kopiec.begin()); //usowam 1 element kopca
+			   // warunek = false;
+				//}
+		}
+		//if(warunek){
+            kopiec.Remove()
+			kopiec.erase(kopiec.begin()); //usowam 1 element kopca
+			for(pomoc = graf_kpp[v]; pomoc; pomoc = pomoc->next)	//dodaje nie odwiedzonych sasiadow do kopca
+				if(!vis[pomoc->v])	
+				{
+					nowy2 = new wezel(*pomoc);
+					kopiec.insert(*nowy2);
+				}
+		//}
+		//warunek=true;
+	}
            
        
 
